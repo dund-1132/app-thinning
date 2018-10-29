28177f319880b585f827ecef4eae67e274803265
Macbook <du.d.ngo@vntrip.vn>
2018-10-29 09:35:38 +0700

    Version 1.0 App thinning

---
+++ AppThinning/AppThinning/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AppThinning
+//
+//  Created by ngodacdu on 10/29/18.
+//  Copyright © 2018 ngodacdu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
